869bdc7c5eaca1147ce12e51776456fc8c48f8ba
ylee <leeyn.shun@gmail.com>
2025-03-13 22:35:30 +0200

    Add shoot animation to player's crosshair. Separate showFeedback() from updateUI(): need to update word when valid target hit and not after checking all targets.

--- shoot.js
+++ shoot.js
@@ -1,57 +1,72 @@
 import { game, GAME_OVER, COMPLETE } from "./main.js";
 import { hideTarget, addTarget } from "./target.js";
-import { updateUI } from "./ui.js";
+import { showFeedback, updateUI } from "./ui.js";
 import { player } from "./player.js";
+import { block } from "./block.js"
 
 const BG_LAYER = document.getElementById('bgLayer');
 const TGT_LAYER = document.getElementById('targetsLayer');
 const WRD_DISPLAY = document.getElementById('word-display');
 const BULLET_HOLES = document.getElementsByClassName('bullet-circle');
 
-const HIT = 'hit';
-const MISS = 'miss';
-const BIM = 'bim';
+const HIT = 'HIT';
+const MISS = 'MISS';
+const BIM = 'BIM';
 
 export function shoot() {
+    player.xAxis.classList.add('shoot');
+    player.yAxis.classList.add('shoot');
+    player.circle.classList.add('shoot');
+
+    setTimeout(() => {
+        player.xAxis.classList.remove('shoot');
+        player.yAxis.classList.remove('shoot');
+        player.circle.classList.remove('shoot');
+    }, 300);  
+
     player.bullets--;
     if (player.bullets <= 0) game.state = GAME_OVER;
 
     let targetHit = false;
     let result = MISS;
 
-    game.targets.forEach((t, i) => {
-        const distance = getDistance(player, t)
-    
-        if (distance <= t.r + 2 && t.active) {
-            targetHit = true;
-            hideTarget(t);
-            let letter = t.letter;
-            result = checkWordCompletion(t.letter);
-    
-            if (result !== MISS) {
-                letter = game.targetList[game.currentTargetIndex];
-                game.currentTargetIndex++;
-                if (result === BIM) {
-                    game.currentWordIndex++;
-                    if (game.currentWordIndex >= game.wordList.length){
-                        game.state = COMPLETE;
+    if (!((player.x >= block.x && player.x <= block.x+150) && (player.y >= block.y && player.y <= block.y+150))){
+        game.targets.forEach((t, i) => {
+            const distance = getDistance(player, t)
+        
+            if (distance <= t.r + 5 && t.active) {
+                targetHit = true;
+                hideTarget(t);
+                let letter = t.letter;
+                result = checkWordCompletion(t.letter);
+        
+                if (result !== MISS) {
+                    letter = game.targetList[game.currentTargetIndex];
+                    game.currentTargetIndex++;
+                    if (result === BIM) {
+                        game.currentWordIndex++;
+                        if (game.currentWordIndex >= game.wordList.length){
+                            game.state = COMPLETE;
+                        };
                     };
+                    showFeedback(result)
+                    player.score += Math.floor(100 * (1+(player.combo/10)));
+                    player.combo++;
                 };
-                player.score += Math.floor(100 * (1+(player.combo/10)));
-                player.combo++;
-            };
-
-            setTimeout(() => {
-                t = addTarget(i, letter);
-            }, 1100);
-        }
-    });
+    
+                setTimeout(() => {
+                    t = addTarget(i, letter);
+                }, 1100);
+            }
+        });
+    }
 
     if (!targetHit || result === MISS){
         player.combo = 0;
+        showFeedback(result);
     }
     addBulletHole(player.x, player.y, targetHit);
-    updateUI(result);
+    updateUI();
 }
 
 function getDistance(player, t){
@@ -109,7 +124,6 @@ async function addBulletHole(x, y, targetHit) {
     setTimeout(() => {
         currentHole.classList.remove('target-removal');
         currentHole.style.display = 'none';
-        currentHole.setAttribute('cy', -100);
     }, 1000);
 
     return currentHole;
